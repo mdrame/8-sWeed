2d406635069edd98776f34a234cfac782549e2bb
mohammed <mdrame113@gmail.com>
2020-01-09 12:54:05 -0800

    local

---
+++ 8'sWeed/8'sWeed/JsonModel.swift
@@ -0,0 +1,15 @@
+//
+//  JsonModel.swift
+//  8'sWeed
+//
+//  Created by Mohammed Drame on 1/6/20.
+//  Copyright © 2020 Mo Drame. All rights reserved.
+//
+
+import Foundation
+
+
+struct DataModel: Decodable{
+    var name : String
+    
+}
--- 8'sWeed/8'sWeed/ViewController.swift
+++ 8'sWeed/8'sWeed/ViewController.swift
@@ -9,12 +9,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        if let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?appid=8b8286cb872c5238a1941724160e04b9&units=metric&q=london") {
+            
+            let session = URLSession(configuration: .default)
+            let task =  session.dataTask(with: url) { (data, respond, error) in
+                if error != nil {
+                    print("Error Occured while fetching data: \(error!.localizedDescription)")
+                } else {
+                    
+                    let decorder = JSONDecoder()
+                    
+                    do {
+                        let decordedData = try decorder.decode(DataModel.self, from: data!)
+                        print(decordedData.name)
+                    } catch {
+                            print("Erroy Occured\(error)")
+                        }
+                    
+                }
+            }
+            
+            task.resume()
+            
+        }
+        
+        
+        
+        
     }
-
-
+    
+    
+    
+    
+    
 }
+
+
+
 

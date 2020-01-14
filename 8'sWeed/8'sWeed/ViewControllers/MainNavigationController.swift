6193051c5170ca899a761152d16fcb6402542e65
mohammed <mdrame113@gmail.com>
2020-01-13 22:46:48 -0800

    local

---
+++ 8'sWeed/8'sWeed/ViewControllers/MainNavigationController.swift
@@ -0,0 +1,30 @@
+//
+//  MainNavigationController.swift
+//  8'sWeed
+//
+//  Created by Mohammed Drame on 1/9/20.
+//  Copyright © 2020 Mo Drame. All rights reserved.
+//
+
+import UIKit
+
+class MainNavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

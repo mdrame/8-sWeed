b1b7fb3ebb1ba0dcaaed3cd93e7758a5e18b9e1b
mohammed <mdrame113@gmail.com>
2020-01-14 20:07:14 -0800

    new project

---
+++ 8'sWeed/8'sWeed/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  8'sWeed
+//
+//  Created by Mohammed Drame on 1/14/20.
+//  Copyright © 2020 Mo Drame. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
